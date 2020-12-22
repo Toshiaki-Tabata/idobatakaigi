7f2279d554a85d0239cc352205831d3c59c5b07e
Toshiaki Tabata <toshiaki.tabata@gmail.com>
2020-12-23 00:10:22 +0900

    setName in japanese.

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import SignIn from './SignIn';
+import { Main } from './Main';
+import { SignIn } from './SignIn';
 
 export const App = () => {
   const [name, setName] = useState('');
   console.log(name);
 
-  return <SignIn setName={setName} />;
+  if (name === '') {
+    return <SignIn setName={setName} />;
+  } else {
+    return <Main name={name} />;
+  }
 };

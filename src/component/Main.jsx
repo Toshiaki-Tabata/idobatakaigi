4311f718676bc08cc920223dacaaeef2b4f1cc8d
Toshiaki Tabata <toshiaki.tabata@gmail.com>
2020-12-23 00:14:56 +0900

    main

---
+++ src/component/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Main = ({name}) => {
+    return (
+        <div>
+            <h1>Main</h1>
+            <p>Your Name is {name}.</p>
+        </div>
+    );
+};

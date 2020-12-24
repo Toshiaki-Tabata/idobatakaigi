269d46cc59dc1b687dec1677af67111e3d58793c
Toshiaki Tabata <toshiaki.tabata@gmail.com>
2020-12-25 06:34:59 +0900

    generate avatar

--- src/component/MessageInputField.jsx
+++ src/component/MessageInputField.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles, Grid, Avatar } from "@material-ui/core";
 
+import {gravatarPath} from '../gravatar';
+
 const useStyles = makeStyles({
     root: {
         gridRow: 2,
@@ -8,13 +10,14 @@ const useStyles = makeStyles({
     }
 })
 
-export const MessageInputField = () => {
+export const MessageInputField = ({name}) => {
     const classes = useStyles();
+    const avatarPath = gravatarPath(name);
     return (
         <div className={classes.root}>
             <Grid container spacing={1}>
                 <Grid item xs={1}>
-                    <Avatar />
+                    <Avatar src={avatarPath} />
                 </Grid>
                 <Grid item xs={10}>
                     入力

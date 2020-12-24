1bbd8bc8519cb592fcf27465c2c2e959f93b814f
Toshiaki Tabata <toshiaki.tabata@gmail.com>
2020-12-25 05:33:16 +0900

    input field grid

--- src/component/MessageInputField.jsx
+++ src/component/MessageInputField.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Grid, Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles({
     root: {
         gridRow: 2,
+        margin: "26px",
     }
 })
 
 export const MessageInputField = () => {
     const classes = useStyles();
     return (
-        <div className={classes.root}>MessageInputField</div>
+        <div className={classes.root}>
+            <Grid container spacing={1}>
+                <Grid item xs={1}>
+                    <Avatar />
+                </Grid>
+                <Grid item xs={10}>
+                    入力
+                </Grid>
+                <Grid item xs={1}>
+                    ボタン
+                </Grid>
+            </Grid>
+        </div>
     );
 }

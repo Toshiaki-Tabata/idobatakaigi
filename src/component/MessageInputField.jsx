8f48c558d8e809b6e1d790dc14b649b737204a33
Toshiaki Tabata <toshiaki.tabata@gmail.com>
2020-12-27 06:56:10 +0900

    message field component

--- src/component/MessageInputField.jsx
+++ src/component/MessageInputField.jsx
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Grid, Avatar } from "@material-ui/core";
 
 import {gravatarPath} from '../gravatar';
+import {MessageField} from './MessageField';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         gridRow: 2,
         margin: "26px",
-    }
-})
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+      },
+}));
 
 export const MessageInputField = ({name}) => {
+    const [text, setText] = useState('');
     const classes = useStyles();
     const avatarPath = gravatarPath(name);
+
+    const onSubmitData = () => {
+        if (text !== '') {
+            console.log('push message to firebase');
+            setText('');
+        };
+    };
+
     return (
         <div className={classes.root}>
-            <Grid container spacing={1}>
+            <form className={classes.form} noValidate
+            　  onSubmit={e => {
+    　              setText(text);
+                    onSubmitData();
+                    e.preventDefault();
+                }}>
+                <Grid container spacing={1}>
                 <Grid item xs={1}>
                     <Avatar src={avatarPath} />
                 </Grid>
                 <Grid item xs={10}>
-                    入力
+                    <MessageField setText={setText} text={text} name={name} />
                 </Grid>
                 <Grid item xs={1}>
                     ボタン
                 </Grid>
             </Grid>
+            </form>
         </div>
     );
 }
